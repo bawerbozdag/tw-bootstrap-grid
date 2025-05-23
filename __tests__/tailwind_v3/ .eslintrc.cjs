eaeb3908534c93a006f5e0c85039a58519a5a3e8
bawerbozdag <bawerbozdag@gmail.com>
2025-05-24 01:25:48 +0300

    chore: add `Tailwind v3` test setup with configuration and utility functions

---
+++ __tests__/tailwind_v3/ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+    },
+    extends: [],
+};
---
+++ __tests__/tailwind_v3/tailwind.config.ts
@@ -0,0 +1,9 @@
+import plugin from "../../src/index";
+
+export default {
+    content: [{ raw: "" }],
+    theme: {
+        extend: {},
+    },
+    plugins: [plugin({})],
+};
