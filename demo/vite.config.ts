5bbcdae4b6b72aa222c7464b4aef2385576ce7eb
bawerbozdag <bawerbozdag@gmail.com>
2025-10-11 20:15:32 +0300

    add custom alias for `tailwind`

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: {
+            // define a custom alias "@tailwind-core" for the Tailwind CSS core file
+            "@tailwind-core": path.resolve(__dirname, "./app/assets/css/tailwind.css"),
+        },
+    },
     plugins: [
         react(),
         tsconfigPaths(),
